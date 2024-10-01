e43223feda167a19e06b16f4d0fbc859a7b9d9a9
shaheemMPM <shaheem.mpm@gmail.com>
2024-10-01 09:58:12 +0530

    Binds to a port

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const net = require("net");
+
+const server = net.createServer((socket) => {
+  socket.on("close", () => {
+    socket.end();
+  });
+});
+
+server.listen(4221, "localhost");
