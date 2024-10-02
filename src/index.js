c7853a02beeb7311f257f647fbaf96c5faf76c6c
shaheemMPM <shaheem.mpm@gmail.com>
2024-10-02 23:34:04 +0530

    Extracts url path

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
+  });
+
+  socket.on("data", (data) => {
+    /**
+     * data will be buffer, need to convert to string before parsing path/headers/body
+     */
+    const rawRequest = data.toString();
+    const requestParts = rawRequest.split("\r\n");
+    const requestLine = requestParts[0];
+
+    const [_requestMethod, requestPath] = requestLine.split(" ");
+
+    const responseStatus = requestPath === "/" ? "200 OK" : "404 Not Found";
+
+    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
   });
 });
 
